b45648071199b2691870bbe4e43349190c665655
Amanda Spader <amanda.spader@ifpr.edu.br>
2026-02-27 17:38:52 -0300

    Finalização de Teste do Formulário simples

--- exemplo03/dadosRecebidos.php
+++ exemplo03/dadosRecebidos.php
@@ -1,5 +1,15 @@
 <?php
 
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
 
+        echo "<h2>Dados Recebidos!</h2>";
+        echo "Seja bem-vindo(a) $nome! <br>";
+        echo "E-mail cadastrado: $email.";
+
+    } else {
+        echo "<h2>Nenhum dado recebido!</h2>";
+    }
 
 ?>
